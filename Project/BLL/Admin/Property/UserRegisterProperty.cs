29ffe1535eaaa577921ec2e4db74688d93899fba
hibanafeesath <haibzzhbz@gmail.com>
2023-03-17 12:53:39 +0530

    Project

---
+++ Project/BLL/Admin/Property/UserRegisterProperty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Admin.Property
+{
+    class UserRegisterProperty
+    {
+        public int Id { get; set; }
+        public string UName { get; set; }
+        public string UAddress { get; set; }
+        public string UPhone { get; set; }
+        public string UPinCode { get; set; }
+        public string UEmail { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Status { get; set; }
+    }
+}
